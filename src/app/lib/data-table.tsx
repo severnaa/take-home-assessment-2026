7d8961d7848f12c65e2f31d85b432fd4be24f931
severnaa <44298139+severnaa@users.noreply.github.com>
2026-02-20 16:36:22 -0800

    Added sorting

--- src/app/lib/data-table.tsx
+++ src/app/lib/data-table.tsx
@@ -1,45 +1,95 @@
 'use client'
 
-import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table'
+import { useReactTable, getCoreRowModel, flexRender, getFilteredRowModel, getSortedRowModel, ColumnDef } from '@tanstack/react-table'
+import { useState } from 'react'
 
-export default function DataTable({data}: {data: string}) {
+const columns: ColumnDef<{[x: string]: {};}, any>[] = [
+  { accessorKey: 'State', header: 'State', sortingFn: 'alphanumeric' },
+  { accessorKey: 'DeadlineInPerson', header: 'DeadlineInPerson', sortingFn: 'datetime' },
+  { accessorKey: 'DeadlineByMail', header: 'DeadlineByMail', sortingFn: 'datetime' },
+  { accessorKey: 'DeadlineOnline', header: 'DeadlineOnline', sortingFn: 'datetime' },
+  { accessorKey: 'ElectionDayRegistration', header: 'ElectionDayRegistration', sortingFn: 'alphanumeric' },
+  { accessorKey: 'OnlineRegistrationLink', header: 'OnlineRegistrationLink', sortingFn: 'alphanumeric' },
+  { accessorKey: 'Description', header: 'Description', sortingFn: 'alphanumeric' },
+]
 
-    const parsedData = JSON.parse(data)
-    const columns = [
-        { accessorKey: 'State', header: 'State' },
-        { accessorKey: 'DeadlineInPerson', header: 'DeadlineInPerson' },
-        { accessorKey: 'DeadlineByMail', header: 'DeadlineByMail' },
-        { accessorKey: 'DeadlineOnline', header: 'DeadlineOnline' },
-        { accessorKey: 'ElectionDayRegistration', header: 'ElectionDayRegistration' },
-        { accessorKey: 'OnlineRegistrationLink', header: 'OnlineRegistrationLink' },
-        { accessorKey: 'Description', header: 'Description' },
-    ]
-    const table = useReactTable({ data: parsedData, columns, getCoreRowModel: getCoreRowModel() })
+export default function DataTable({data}: {data: any}) {
+  const [globalFilter, setGlobalFilter] = useState<any>([])
+  const table = useReactTable({ 
+    data, 
+    columns, 
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    globalFilterFn: 'includesString',
+    state: {
+      globalFilter,
+    },
+    onGlobalFilterChange: setGlobalFilter
+    })
 
-    return (
-        <table>
-          <thead>
-            {table.getHeaderGroups().map((hg) => (
-              <tr key={hg.id}>
-                {hg.headers.map((header) => (
-                  <th key={header.id}>
-                    {flexRender(header.column.columnDef.header, header.getContext())}
+  console.log("Hello!!!")
+
+  return (
+    <div>
+      <input
+        value={globalFilter || ''}
+        onChange={e => table.setGlobalFilter(String(e.target.value))}
+        placeholder="Search..."
+      />
+      <table>
+      <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div
+                        className={
+                          header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : ''
+                        }
+                        onClick={header.column.getToggleSortingHandler()}
+                        title={
+                          header.column.getCanSort()
+                            ? header.column.getNextSortingOrder() === 'asc'
+                              ? 'Sort ascending'
+                              : header.column.getNextSortingOrder() === 'desc'
+                                ? 'Sort descending'
+                                : 'Clear sort'
+                            : undefined
+                        }
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                        {{
+                          asc: ' 🔼',
+                          desc: ' 🔽',
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    )}
                   </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )
-  }
+                )
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    )
+}
--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -5,11 +5,13 @@ const LOCAL_API = 'http://localhost:3000/api/state_data'
 export default async function Page() {
     const response = await fetch(LOCAL_API);
     const data = await response.text();
+    const parsedData = JSON.parse(data)
+
 
     return (
         <div>
             <h1>Voter Registration Deadlines By State</h1>
-            <DataTable data={data}/>
+            <DataTable data={parsedData}/>
         </div>
     );
   }
